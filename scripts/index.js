2d08a7ead684c53176acdc91c10c08ccaf0d3de7
yara-almeida <yaraalmeida.dev@gmail.com>
2024-09-01 16:15:23 -0300

    Atualização

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 const navbar = document.querySelector(".navbar");
 const mobileNavbar = document.querySelector(".navbar__mobile");
-// const button = document.querySelector(".burguer");
+const button = document.querySelector(".burguer");
 
 button.addEventListener("click", function () {
     mobileNavbar.classList.toggle("active");
