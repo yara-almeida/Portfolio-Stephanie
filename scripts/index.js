149906ee4da25444cd3461f052f1267fe37da5d7
yara-almeida <yaraalmeida.dev@gmail.com>
2024-09-01 16:05:55 -0300

    first code

---
+++ scripts/index.js
@@ -0,0 +1,20 @@
+const navbar = document.querySelector(".navbar");
+const mobileNavbar = document.querySelector(".navbar__mobile");
+// const button = document.querySelector(".burguer");
+
+button.addEventListener("click", function () {
+    mobileNavbar.classList.toggle("active");
+});
+
+window.addEventListener("scroll", function () {
+    if (this.window.scrollY > 0) return navbar.classList.add("active");
+    return navbar.classList.remove("active");
+});
+
+let btn = document.getElementById("show-or-hide");
+
+btn.addEventListener("click", function () {
+    let container = document.getElementById("container");
+
+    container.classList.toggle("hide");
+});
